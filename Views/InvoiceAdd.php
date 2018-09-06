b708f306896c7f9ce06430bb7450813e196796c4
Sammuel JANSSENS <sammueljnssns@gmail.com>
2018-09-06 11:21:53 +0200

    Add Invoice Front integr DONE

--- Views/InvoiceAdd.php
+++ Views/InvoiceAdd.php
@@ -1,26 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	<form action="/facture-add" method="POST">
-		<label for="dateofissue">Date d'émission :</label>
-		<input type="date" name="date">
-		<input hidden name="id" value="<?php echo $_GET['id'] ?>">
-		<label for="">Objets :</label>
-		<input type="text" name="object" value="<?php echo UpdateInvoice::showInvoiceInfo('object') ?>">
-		<label for="">Société</label>
-		<select name="company" id="">
-			<?php echo UpdateInvoice::showAllCompagnies(); ?>
-		</select>
-		<label for="">Personne de contact</label>
-		<select name="contactperson" id="">
-			<?php echo UpdateInvoice::showAllPersones(); ?>
-		</select>
-		<input type="submit" value="Ajouter">
-	</form>
-	<a href="/dashboard">Retourner au dashboard</a>
-</body>
+    <head>
+        <title>Nouvelle Facture</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="../assets/images/CogipLogo.svg"/>
+        <link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/fonts/iconic/css/material-design-iconic-font.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/util.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script defer src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+    </head>
+    <body>
+        <nav>
+            <div class="nav-wrapper">
+                <img class="logo" src="assets/images/CogipLogo.svg" alt="logo">
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="personnes.html">Personnes</a></li>
+                    <li><a href="factures.html">Factures</a></li>
+                    <li><a href="societes.html">Sociétés</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="wrap">  
+            <div class="title-back">
+                <a href="acceuil.html" class="btn waves-effect waves-light red right">Retour</a>
+                <h2 style="font-size: 30px; margin-top: 150px">Création d'une nouvelle facture</h2>
+            </div>
+            <form  action="/facture-add" method="POST">
+	            <div class="row">
+	                <div class="input-field col s6">
+	                    <input value="" id="date" type="date" class="validate" name="date">
+	                    <label class="active" for="date">Date d'émission</label>
+	                </div>
+	            </div> 
+	            <div class="row">
+	                <div class="input-field col s6">
+	                    <input value="" id="objet" name="object" type="text" class="validate">
+	                    <label class="active" for="objet">Objet de la facture</label>
+	                </div>
+	            </div>
+	            <div class="row">
+	                <div class="input-field col s6">
+	                    <label class="active" for="objet">Personne de contact</label>
+	                	<select class="form-control mt-2" name="contactperson" id="">
+	                		<?php echo UpdateInvoice::showAllPersones(); ?>
+	                	</select>
+	                </div>
+	            </div>
+	            <div class="row">
+	                <div class="input-field col s6">
+	                    <label class="active" for="company">Société</label>
+	                    <select name="company" class="form-control mt-2" id="company">
+	                    	<?php echo UpdateInvoice::showAllCompagnies(); ?>
+	                    </select>
+	                </div>
+	            </div> 
+	            <button class="btn waves-effect waves-light red right" type="submit" name="action">
+	                Envoyer
+	            </button>
+            </form>
+        </div>
+    </body>
 </html>
